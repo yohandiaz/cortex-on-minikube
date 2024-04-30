9addcb08ce112117ef56102e3f56d979801c412c
Yohan Diaz <yohan.diaz5632@gmail.com>
2024-04-30 01:13:59 +0000

    Adding helm to dependencies scripts

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,7 +11,7 @@ if [[ ! -x "$(command -v kubectl)" ]]; then
 
     # Download and Install Kubectl
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
 
     # Check for errors in installation
     if [[ $? -eq 0 && -x $(command -v kubectl) ]]; then
@@ -31,7 +31,7 @@ if [[ ! -x "$(command -v minikube)" ]]; then
 
     # Download and Install Minikube
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-    sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+    install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
     # Check for errors in installation
     if [[ $? -eq 0 && -x $(command -v minikube) ]]; then
@@ -49,24 +49,24 @@ if [[ ! -x "$(command -v docker)" ]]; then
     # Install Docker
     echo "Installing Docker.."
     # Uninstall old conflicting packages
-    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
 
     # Add Docker's official GPG key
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    apt-get update
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
 
     # Install the latest docker version
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Check for errors in installation
     if [[ $? -eq 0 && -x $(command -v docker) ]]; then
@@ -79,5 +79,28 @@ else
     echo "Docker is already installed. Skipping installation..."
 fi
 
+# Check helm isn't already installed
+if [[ ! -x "$(command -v helm)" ]]; then
+    # Install Helm
+    echo "Installing Helm..."
+
+    # Download and Install Helm
+    curl -LO https://get.helm.sh/helm-v3.14.4-linux-amd64.tar.gz
+    tar -zxvf helm-v3.14.4-linux-amd64.tar.gz
+    mv linux-amd64/helm /usr/local/bin/helm
+
+    # Cleanup the downloaded files
+    rm -rf linux-amd64 helm-v3.14.4-linux-amd64.tar.gz
+
+    # Check for errors in installation
+    if [[ $? -eq 0 && -x $(command -v helm) ]]; then
+        echo "Helm installed successfully!"
+    else
+        echo "Failed to install Helm. Check for errors above. Exiting..."
+        exit 1
+    fi
+else
+    echo "Helm is already installed. Skipping installation..."
+fi
 # Dependencies installed successfully
 echo -e "Dependencies installed successfully!"
--- remove_dependencies.sh
+++ remove_dependencies.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # This script removes Docker, Minikube and Kubectl from the system, ask user for confirmation before proceeding
-read -p "Are you sure you want to remove Docker, Minikube, and Kubectl? (yes/no): " answer
-if [ "$answer" != "yes" ]; then
-    echo "Aborted."
-    exit 1
+read -p "Are you sure you want to remove Docker, Minikube, Helm, Kubectl and all of their configuration? (yes/no): " answer
+if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+    echo "Proceeding with removal..."
+else
+    echo "Aborting..."
+    exit 0
 fi
 
-
-# Remove Minikube and Kubectl binaries
-echo "Removing Minikube and Kubectl binaries..."
-rm /usr/local/bin/minikube /usr/local/bin/kubectl
+# Remove Minikube, Kubectl and Helm binaries
+echo "Removing Minikube, Kubectl and Helm binaries..."
+rm /usr/local/bin/minikube /usr/local/bin/kubectl /usr/local/bin/helm
 
 # Remove Minikube and Kubectl directories
-#echo "Removing Minikube and Kubectl directories..."
-#rm -rf ~/.minikube ~/.kube
+echo "Removing Minikube and Kubectl directories..."
+rm -rf ~/.minikube ~/.kube
 
 # Remove package dependencies
 echo "Removing package dependencies for the cluster..."
@@ -33,9 +34,9 @@ echo "Removing Docker public key..."
 rm /etc/apt/keyrings/docker.asc
 
 # Check docker, minikube and kubectl are removed
-if [ -x "$(command -v docker)" ] || [ -x "$(command -v kubectl)" ]; then
+if [[ -x "$(command -v docker)" || -x "$(command -v kubectl)" || -x "$(command -v minikube)" || -x "$(command -v helm)" ]]; then
     # Echo in red
-    echo -e "Failed to remove dependencies. Check for errors above"
+    echo -e "Failed to remove dependencies. Check for errors above. Exiting..."
 else
     echo "Dependencies removed successfully!"
 fi
