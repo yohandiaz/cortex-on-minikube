e285dda1f180d7e5a068dd5ffcdddac0b8f70347
Yohan Diaz <yohan.diaz5632@gmail.com>
2024-04-30 01:45:38 +0000

    Removing configuration removal from script

--- remove_dependencies.sh
+++ remove_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script removes Docker, Minikube and Kubectl from the system, ask user for confirmation before proceeding
-read -p "Are you sure you want to remove Docker, Minikube, Helm, Kubectl and all of their configuration? (yes/no): " answer
+read -p "Are you sure you want to remove Docker, Minikube, Helm, Kubectl? (yes/no): " answer
 if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
     echo "Proceeding with removal..."
 else
@@ -14,8 +14,8 @@ echo "Removing Minikube, Kubectl and Helm binaries..."
 rm /usr/local/bin/minikube /usr/local/bin/kubectl /usr/local/bin/helm
 
 # Remove Minikube and Kubectl directories
-echo "Removing Minikube and Kubectl directories..."
-rm -rf ~/.minikube ~/.kube
+#echo "Removing Minikube and Kubectl directories..."
+#rm -rf ~/.minikube ~/.kube
 
 # Remove package dependencies
 echo "Removing package dependencies for the cluster..."
